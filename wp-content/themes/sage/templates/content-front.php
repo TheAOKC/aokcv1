a19fc436a409c173558809e13eaabc2af7105d2a
sonjudan <son2x.judan@gmail.com>
2017-05-24 13:58:54 +0800

    what we what html

--- wp-content/themes/sage/templates/content-front.php
+++ wp-content/themes/sage/templates/content-front.php
@@ -97,22 +97,23 @@
     <div class="to-change-holder clearfix">
         <ul>
             <li>
-                <a href="#">
+
+                <a href="#" style="background-image: url('https://kungfuture.org/media/conflict.jpg');">
                     <h3>Disaster Relief</h3>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="#" style="background-image: url('http://themekalia.com/wp/humanwelfare/wp-content/uploads/2016/12/featured-image-14-795x399.jpg');">
                     <h3>Medical</h3>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="#" style="background-image: url('http://themekalia.com/wp/humanwelfare/wp-content/uploads/2016/12/featured-image-10.jpg');">
                     <h3>Education</h3>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="#" style="background-image: url('http://themekalia.com/wp/humanwelfare/wp-content/uploads/2016/12/featured-image-14-795x399.jpg');">
                     <h3>Child Trafficking</h3>
                 </a>
             </li>
